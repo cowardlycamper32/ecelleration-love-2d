538977bace3d1ff249692145026fb946c1a1fe5f
cowardlycamper32 <g.hickman2022@gmail.com>
2025-01-31 16:04:56 +0000

    almost fixed my changes

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
-objectID = 0
+staticObjectID = 0
+controllableObjectID = 1
+movingObjectID = 1
 colors = {
     black = {
         r = 0,
@@ -14,9 +16,19 @@ colors = {
         r = 1,
         g = 0,
         b = 0
+    },
+    green = {
+        r = 0,
+        g = 1,
+        b = 0
+    },
+    blue = {
+        r = 0,
+        g = 0,
+        b = 1
     }
 }
-objects = {
+StaticObjects = {
     {
         ID = 0,
         reference = "gameWindow",
@@ -40,35 +52,38 @@ objects = {
         }
     }
 }
+ControllableObjects = {}
+MovingObjects = {}
 
-InitialEnemies = {
-    ID = 0,
-    reference = "enemy01",
-    name = "Enemy",
-    Type = "elipse",
-    Data = {
-        position = {
-            x = math.random(0, objects[1].Data.size.width - 20 / 2),
-            y = math.random(0, objects[1].Data.size.width - 20 / 2)
-        },
-        size = {
-            width = 20 / 2,
-            height = 20 / 2
-        },
-        direction = {
-            x = 0,
-            y = 0
-        },
-        speed = 0,
-        color = colors.red
-    }
 
-}
+function addStaticObject(reference, name, Type, drawType, objectData)
+    staticObjectID = staticObjectID + 1
+    table.insert(StaticObjects, {
+        ID = StaticObjectID,
+        reference = reference,
+        name = name,
+        Type = Type,
+        drawType = drawType,
+        Data = objectData
+    })
+end
 
-function addObject(reference, name, Type, drawType, objectData) 
-    objectID = objectID + 1
-    table.insert(objects, {
-        ID = objectID,
+function addControllableObject(reference, name, Type, drawType, objectData)
+    controllableObjectID = controllableObjectID + 1
+    table.insert(ControllableObjects, {
+        ID = controllableObjectID,
+        reference = reference,
+        name = name,
+        Type = Type,
+        drawType = drawType,
+        Data = objectData
+    })
+end
+
+function addMovingObject(reference, name, Type, drawType, objectData)
+    movingObjectID = movingObjectID + 1
+    table.insert(MovingObjects, {
+        ID = movingObjectID,
         reference = reference,
         name = name,
         Type = Type,
@@ -125,16 +140,19 @@ function boundsCheck(object)
     if object.Data.position.x < 0 then
         object.Data.position.x = 0
         object.Data.direction.x = 0
-    elseif object.Data.position.x > objects[2].Data.size.width - object.Data.size.width then
-        object.Data.position.x = objects[2].Data.size.width - object.Data.size.width
+    elseif object.Data.position.x > ControllableObjects[1].Data.size.width - object.Data.size.width then
+        object.Data.position.x = ControllableObjects[1].Data.size.width - object.Data.size.width
         object.Data.direction.x = 0
     end
     if object.Data.position.y < 0 then
         object.Data.position.y = 0
         object.Data.direction.y = 0
-    elseif object.Data.position.y > objects[2].Data.size.height - object.Data.size.height then
-        object.Data.position.y = objects[2].Data.size.height - object.Data.size.height
+    elseif object.Data.position.y > ControllableObjects[1].Data.size.height - object.Data.size.height then
+        object.Data.position.y = ControllableObjects[1].Data.size.height - object.Data.size.height
         object.Data.direction.y = 0
+    end
+    if object.reference == "enemies" then
+        return
     end
         
 end
@@ -144,22 +162,13 @@ function updateEnemy(enemy, dt)
     enemy.Data.position.y = enemy.Data.position.y + (enemy.Data.speed * enemy.Data.direction.y) * dt
 end
 
-function generateEnemy(enemy) 
-    
-end
 
 function doUpdate(object, dt)
     if object.ID == 0 then
         return
     end
-    if object.reference == "Player" then
-        playerInputManager(object)
-        move(object, dt)
-        boundsCheck(object)
-    elseif object.reference == "enemies" then
-        for _, enemy in pairs(object.Data) do
-            updateEnemy(enemy, dt)
-        end
+    if object.Type == "enemy" then
+        updateEnemy(object, dt)
     end
         
 end
@@ -168,35 +177,33 @@ function doDraw(object)
     if object.ID == 0 then
         return
     end
-    if not(object.reference == "enemies") then
-        love.graphics.setColor(object.Data.color.r, object.Data.color.g, object.Data.color.b)
-    else
-        for _, enemy in pairs(object) do
-            doDraw(enemy)
-        end
+    love.graphics.setColor(object.Data.color.r, object.Data.color.g, object.Data.color.b)
+    if object.Type == "enemy" then
+        object.Type = "ellipse"
     end
+
     if object.Type == "rectangle" then
 
         love.graphics.rectangle(object.drawType, object.Data.position.x, object.Data.position.y, object.Data.size.width, object.Data.size.width)
 
-    elseif object.Type == "elipse" then
-        love.graphics.elipse(object.drawType, object.Data.position.x, object.Data.position.y, object.Data.size.width / 2, object.Data.size.height / 2)
+    elseif object.Type == "ellipse" then
+        love.graphics.ellipse(object.drawType, object.Data.position.x, object.Data.position.y, object.Data.size.width / 2, object.Data.size.height / 2)
     elseif object.Type == "text" then
         if object.reference == "playerVel" then
-            love.graphics.print(objects[2].Data.direction.x .. "," .. objects[2].Data.direction.y, object.Data.position.x, object.Data.position.y)
+            love.graphics.print(ControllableObjects[1].Data.direction.x .. "," .. ControllableObjects[1].Data.direction.y, object.Data.position.x, object.Data.position.y)
         end
     end
 
 
-    love.graphics.setColor(objects[1].Data.color.r, objects[1].Data.color.g, objects[1].Data.color.b)
+    love.graphics.setColor(StaticObjects[1].Data.color.r, StaticObjects[1].Data.color.g, StaticObjects[1].Data.color.b)
 end
 
 function love.load()
-    objects[1].Data.size.width, objects[1].Data.size.height = love.graphics.getDimensions()
-    addObject("Player", "Player", "rectangle", "fill", {
+    StaticObjects[1].Data.size.width, StaticObjects[1].Data.size.height = love.graphics.getDimensions()
+    addControllableObject("Player", "Player", "rectangle", "fill", {
         position = {
-            x = objects[1].Data.size.width / 2,
-            y = objects[1].Data.size.height / 2
+            x = StaticObjects[1].Data.size.width / 2,
+            y = StaticObjects[1].Data.size.height / 2
         },
         size = {
             width = 20,
@@ -209,8 +216,23 @@ function love.load()
         speed = 1000,
         color = colors.white
     })
-    addObject("enemies", "Enemies", "table", 0, {InitialEnemies})
-    addObject("playerVel", "Player Velocity thingie", "text" , 0, {
+    addMovingObject("enemy01", "Enemy#1", "enemy", "fill", {
+        position = {
+            x = math.random(0, StaticObjects[1].Data.size.width - 20 / 2),
+            y = math.random(0, StaticObjects[1].Data.size.height - 20 / 2)
+        },
+        size = {
+            width = 20 / 2,
+            height = 20 / 2
+        },
+        direction = {
+            x = 0,
+            y = 0
+        },
+        speed = 20,
+        color = colors.red
+    })
+    addStaticObject("playerVel", "Player Velocity thingie", "text" , 0, {
         position = {
             x = 0,
             y = 0
@@ -229,13 +251,30 @@ function love.load()
 end
 
 function love.draw()
-    for _, object in ipairs(objects) do
+    for _, object in ipairs(StaticObjects) do
+        doDraw(object)
+    end
+    for _, object in ipairs(ControllableObjects) do
+        doDraw(object)
+    end
+    for _, object in ipairs(MovingObjects) do
         doDraw(object)
     end
 end 
 
 function love.update(dt)
-    for _, object in ipairs(objects) do
+    for _, object in ipairs(StaticObjects) do
         doUpdate(object, dt)
+    end
+    for _, object in ipairs(ControllableObjects) do
+        playerInputManager(object)
+        doUpdate(object, dt)
+        move(object, dt)
+        boundsCheck(object)
+    end
+    for _, object in ipairs(MovingObjects) do
+        doUpdate(object, dt)
+        move(object, dt)
+        boundsCheck(object)
     end
 end 
