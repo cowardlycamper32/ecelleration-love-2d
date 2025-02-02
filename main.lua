8628ce4b0aa9951dff0aa096de5a4ce8fb8ee9a5
GHickman <g.hickman2022@gmail.com>
2025-02-02 00:51:46 +0000

    ENEMIES CAN HONE MOVE RANDOMLY AND REFACTORED SMALL THINGS FOR EASE OF USE AAA

--- main.lua
+++ main.lua
@@ -55,6 +55,8 @@ StaticObjects = {
 ControllableObjects = {}
 MovingObjects = {}
 
+enemyStateText = "neutral"
+
 
 function addStaticObject(reference, name, Type, drawType, objectData)
     staticObjectID = staticObjectID + 1
@@ -105,30 +107,29 @@ function playerInputManager(player)
     if love.keyboard.isDown("d") then
         player.Data.direction.x = player.Data.direction.x + 1
     end
-    if player.Data.direction.y > 1 then
-        player.Data.direction.y = 1
+    if love.keyboard.isDown("w") and love.keyboard.isDown("s") then
+        player.Data.direction.y = 0
     end
-    if player.Data.direction.y < -1 then
-        player.Data.direction.y = -1
+    if love.keyboard.isDown("a") and love.keyboard.isDown("d") then
+        player.Data.direction.x = 0
     end
-    if player.Data.direction.x > 1 then
-        player.Data.direction.x = 1
-    end
-    if player.Data.direction.x < -1 then
-        player.Data.direction.x = -1
-    end
+    fixRandomAcceleration(player)
     if not(love.keyboard.isDown("w")) and not(love.keyboard.isDown("s")) then
         player.Data.direction.y = 0
     end
     if not(love.keyboard.isDown("a")) and not(love.keyboard.isDown("d")) then
         player.Data.direction.x = 0
     end
-    local length = math.sqrt(player.Data.direction.x^2 + player.Data.direction.y^2)
-    if length > 0 then
-        player.Data.direction.x = player.Data.direction.x / length
-        player.Data.direction.y = player.Data.direction.y / length
-    end
+    fixVectors(player)
     
+end
+
+function fixVectors(object)
+    local length = math.sqrt(object.Data.direction.x^2 + object.Data.direction.y^2)
+    if length > 0 then
+        object.Data.direction.x = object.Data.direction.x / length
+        object.Data.direction.y = object.Data.direction.y / length
+    end
 end
 
 function move(object, dt) 
@@ -140,35 +141,82 @@ function boundsCheck(object)
     if object.Data.position.x < 0 then
         object.Data.position.x = 0
         object.Data.direction.x = 0
-    elseif object.Data.position.x > ControllableObjects[1].Data.size.width - object.Data.size.width then
-        object.Data.position.x = ControllableObjects[1].Data.size.width - object.Data.size.width
+    elseif object.Data.position.x > StaticObjects[1].Data.size.width - object.Data.size.width then
+        object.Data.position.x = StaticObjects[1].Data.size.width - object.Data.size.width
         object.Data.direction.x = 0
     end
     if object.Data.position.y < 0 then
         object.Data.position.y = 0
         object.Data.direction.y = 0
-    elseif object.Data.position.y > ControllableObjects[1].Data.size.height - object.Data.size.height then
-        object.Data.position.y = ControllableObjects[1].Data.size.height - object.Data.size.height
+    elseif object.Data.position.y > StaticObjects[1].Data.size.height - object.Data.size.height then
+        object.Data.position.y = StaticObjects[1].Data.size.height - object.Data.size.height
         object.Data.direction.y = 0
-    end
-    if object.reference == "enemies" then
-        return
     end
         
 end
 
-function updateEnemy(enemy, dt)
-    enemy.Data.position.x = enemy.Data.position.x + (enemy.Data.speed * enemy.Data.direction.x) * dt
-    enemy.Data.position.y = enemy.Data.position.y + (enemy.Data.speed * enemy.Data.direction.y) * dt
+function fixRandomAcceleration(object)
+    if object.Data.direction.x > 1 then
+        object.Data.direction.x = 1
+    elseif object.Data.direction.x < -1 then
+        object.Data.direction.x = -1
+    end
+    if object.Data.direction.y > 1 then
+        object.Data.direction.y = 1
+    elseif object.Data.direction.y < -1 then
+        object.Data.direction.y = -1
+    end
+end
+xValue = 50
+yValue = 50
+resetValue = 200
+honeValue = 1
+function updateEnemy(enemy)
+    if math.random(1, xValue) == 50 then
+        x1 = math.random(-1, 1)
+        enemy.Data.direction.x = x1
+        StaticObjects[3].Data.contents = "x - moving " .. x1
+    end
+    if math.random(1, yValue) == 50 then
+        y1 = math.random(-1, 1)
+        enemy.Data.direction.y = y1
+        StaticObjects[4].Data.contents = "y - moving " .. y1
+    end
+    if math.random(1, resetValue) == 200 then
+        enemy.Data.direction.x = 0
+        enemy.Data.direction.y = 0
+        StaticObjects[3].Data.contents = "x - neutral"
+        StaticObjects[4].Data.contents = "y - neutral"
+    end
+    if math.random(honeValue, 1000) == 1000 then
+        honeValue = 1000
+        StaticObjects[3].Data.contents = "x - HONE"
+        StaticObjects[4].Data.contents = "y - HONE"
+        xValue = 1
+        yValue = 1
+        resetValue = 1
+        if enemy.Data.position.x - enemy.Data.size.width / 2 > ControllableObjects[1].Data.position.x then
+            enemy.Data.direction.x = enemy.Data.direction.x - 1
+        elseif enemy.Data.position.x - enemy.Data.size.width / 2 < ControllableObjects[1].Data.position.x then
+            enemy.Data.direction.x = enemy.Data.direction.x + 1
+        elseif enemy.Data.position.x - enemy.Data.size.width / 2 == ControllableObjects[1].Data.position.x then
+            enemy.Data.direction.x = 0
+        end
+        if enemy.Data.position.y - enemy.Data.size.height / 2 > ControllableObjects[1].Data.position.y then
+            enemy.Data.direction.y = enemy.Data.direction.y - 1
+        elseif enemy.Data.position.y - enemy.Data.size.height / 2 < ControllableObjects[1].Data.position.y then
+            enemy.Data.direction.y = enemy.Data.direction.y + 1
+        elseif enemy.Data.position.y - enemy.Data.size.height / 2 == ControllableObjects[1].Data.position.y then
+            enemy.Data.direction.y = 0
+        end
+    end
+    fixRandomAcceleration(enemy)
+    fixVectors(enemy)
 end
 
-
-function doUpdate(object, dt)
+function doUpdate(object, dt) -- WTF DOES THIS DOOO?????
     if object.ID == 0 then
         return
-    end
-    if object.Type == "enemy" then
-        updateEnemy(object, dt)
     end
         
 end
@@ -178,9 +226,6 @@ function doDraw(object)
         return
     end
     love.graphics.setColor(object.Data.color.r, object.Data.color.g, object.Data.color.b)
-    if object.Type == "enemy" then
-        object.Type = "ellipse"
-    end
 
     if object.Type == "rectangle" then
 
@@ -191,7 +236,10 @@ function doDraw(object)
     elseif object.Type == "text" then
         if object.reference == "playerVel" then
             love.graphics.print(ControllableObjects[1].Data.direction.x .. "," .. ControllableObjects[1].Data.direction.y, object.Data.position.x, object.Data.position.y)
+        else
+            love.graphics.print(object.Data.contents, object.Data.position.x, object.Data.position.y)
         end
+        
     end
 
 
@@ -213,17 +261,17 @@ function love.load()
             x = 0,
             y = 0
         },
-        speed = 1000,
+        speed = 20,
         color = colors.white
     })
-    addMovingObject("enemy01", "Enemy#1", "enemy", "fill", {
+    addMovingObject("enemy01", "Enemy#1", "ellipse", "fill", {
         position = {
-            x = math.random(0, StaticObjects[1].Data.size.width - 20 / 2),
-            y = math.random(0, StaticObjects[1].Data.size.height - 20 / 2)
+            x = math.random(0, StaticObjects[1].Data.size.width - 20),
+            y = math.random(0, StaticObjects[1].Data.size.height - 20)
         },
         size = {
-            width = 20 / 2,
-            height = 20 / 2
+            width = 20,
+            height = 20
         },
         direction = {
             x = 0,
@@ -247,6 +295,40 @@ function love.load()
         },
         speed = 0,
         color = colors.white
+    })
+    addStaticObject("enemyStateX", "Enemy State X Display", "text", 0, {
+        position = {
+            x = StaticObjects[1].Data.size.width / 2 - 70,
+            y = StaticObjects[1].Data.size.width / 2
+        },
+        size = {
+            width = 0,
+            height = 0
+        },
+        direction = {
+            x = 0,
+            y = 0
+        },
+        speed = 0,
+        color = colors.white,
+        contents = "y - neutral"
+    })
+    addStaticObject("enemyStateY", "Enemy State Y Display", "text", 0, {
+        position = {
+            x = StaticObjects[1].Data.size.width / 2 + 60,
+            y = StaticObjects[1].Data.size.width / 2
+        },
+        size = {
+            width = 0,
+            height = 0
+        },
+        direction = {
+            x = 0,
+            y = 0
+        },
+        speed = 0,
+        color = colors.white,
+        contents = "x - neutral"
     })
 end
 
@@ -274,6 +356,7 @@ function love.update(dt)
     end
     for _, object in ipairs(MovingObjects) do
         doUpdate(object, dt)
+        updateEnemy(object)
         move(object, dt)
         boundsCheck(object)
     end
